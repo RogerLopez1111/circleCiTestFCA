2dafb221113fba2d800e156cd215590066a9a7c0
Roger Lopez Martinez <75762784+RogerLopez1111@users.noreply.github.com>
2024-12-05 09:13:42 -0600

    Update crudTest.java

--- src/test/java/com/fca/calidad/funcionales/crudTest.java
+++ src/test/java/com/fca/calidad/funcionales/crudTest.java
@@ -58,13 +58,7 @@ public class crudTest {
     assertEquals("Nice one!", confirmationMessage.getText());
   }
   
-  @Test
-  public void testDelete() throws Exception {
-    driver.get("https://mern-crud-mpfr.onrender.com/");
-    driver.findElement(By.xpath("//div[@id='root']/div/div[2]/table/tbody/tr/td[5]/button[2]")).click();
-    driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Roger Lopez Martinez'])[2]/following::/html/body/div[3]/div/div[3]/button[1]")).click();
-    
-  }
+
   
   @Test
   public void testEdit() throws Exception {
@@ -77,6 +71,13 @@ public class crudTest {
     driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Gender'])[2]/following::div[2]")).click();
     driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Male'])[1]/following::div[2]")).click();
     driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Woah!'])[1]/following::button[1]")).click();
+  }
+
+  @Test
+  public void testDelete() throws Exception {
+    driver.get("https://mern-crud-mpfr.onrender.com/");
+    driver.findElement(By.xpath("//div[@id='root']/div/div[2]/table/tbody/tr/td[5]/button[2]")).click();
+    driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Roger Lopez Martinez'])[2]/following::button[1]")).click();    
   }
   
   @After
