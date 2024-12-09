ae31b824d374714c117d08b3ed47865d8f48a671
Roger Lopez Martinez <75762784+RogerLopez1111@users.noreply.github.com>
2024-12-09 09:58:09 -0600

    Update crudTest.java

--- src/test/java/com/fca/calidad/funcionales/crudTest.java
+++ src/test/java/com/fca/calidad/funcionales/crudTest.java
@@ -7,6 +7,7 @@ import static org.junit.Assert.*;
 import static org.hamcrest.CoreMatchers.*;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
